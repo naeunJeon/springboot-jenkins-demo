58de2cc8647af099dd1e8eb09349bea0a6f4df87
naeun <mika94322@gmail.com>
2018-12-02 14:15:48 +0900

    Initial commit

---
+++ src/main/java/com/example/springbootjenkinsdemo/SpringbootJenkinsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootjenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJenkinsDemoApplication.class, args);
+	}
+}
